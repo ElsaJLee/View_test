8398dab2e478dc5a7540b0d773a9df4fa01401f0
mochi <n1046746@ntub.edu.tw>
2019-01-23 18:32:02 +0800

    Initial Commit

---
+++ View_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  View_test
+//
+//  Created by 吳佳臻 on 2019/1/23.
+//  Copyright © 2019 ChiaChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
